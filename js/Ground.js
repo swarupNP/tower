fb3dd8e8f5dd48cd3db8dbc2ed0c8f86e1d20157
swarupNP <81356313+swarupNP@users.noreply.github.com>
2021-09-20 15:31:50 +0530

    Add files via upload

---
+++ js/Ground.js
@@ -0,0 +1,14 @@
+class Ground {
+    constructor(){
+        var ground_options={
+            isStatic:true
+        }
+        this.ground=Bodies.rectangle(450,390,900,20,ground_options)
+        World.add(world,this.ground)
+    }
+    display(){
+        fill("green")
+        rectMode(CENTER)
+        rect(this.ground.position.x,this.ground.position.y,900,20)
+    }
+}
